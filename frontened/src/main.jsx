ef96fb1364101d43fbf93af71512076e00bbf094
SonalOmar <96885841+SonalOmar@users.noreply.github.com>
2024-07-12 18:37:33 +0530

    build food item component

--- frontened/src/main.jsx
+++ frontened/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <StoreContextProvider>
-  <App />
-  </StoreContextProvider>
-  </BrowserRouter>
-    
+<BrowserRouter>
+<StoreContextProvider>
+<App />
+</StoreContextProvider>
+
+</BrowserRouter>
+ 
  
 )
